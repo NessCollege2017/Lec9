680ce64492c96d55e54c1cfaff1b62831b770228
nesscollege2017 <nesscollege2017@gmail.com>
2017-01-31 18:10:10 +0200

    Initial commit

---
+++ src/edu/tomer/java/IO.java
@@ -0,0 +1,96 @@
+package edu.tomer.java;
+
+import java.util.Scanner;
+
+public class IO {
+    static Scanner scan = new Scanner(System.in);
+    /**
+     * This method prints an array of integers
+     * @param arr an integer array that we want to print
+     */
+    static void print(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("%4d", arr[i]);
+        }
+        System.out.println();
+    }
+    static void print(int[][] arr){
+        for (int i = 0; i < arr.length; i++) {
+            print(arr[i]);
+        }
+    }
+
+    static void print(String[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("%4s", arr[i]);
+        }
+        System.out.println();
+    }
+    static void print(String[][] arr){
+        for (int i = 0; i < arr.length; i++) {
+            print(arr[i]);
+        }
+    }
+
+    static void printBoard(String[] arr){
+        System.out.printf("|");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("%4s|", arr[i]);
+        }
+        System.out.printf("\n________________\n");
+    }
+    static void printBoard(String[][] arr){
+        System.out.printf("\n________________\n");
+        for (int i = 0; i < arr.length; i++) {
+            printBoard(arr[i]);
+        }
+    }
+
+    static int getInt(String prompt){
+        System.out.println(prompt);
+        int n = scan.nextInt(); //alt + Enter here...
+        return n;
+    }
+
+    static String getString(String prompt){
+        System.out.println(prompt);
+        return scan.next();
+    }
+
+    static int[] getIntArray(String prompt){
+        //ask the user for the size:
+        int n = getInt("Enter the array size");
+        //init an array of the requested size:
+        int[] result = new int[n];
+        //loop through the array and fill it:
+        for (int i = 0; i < n; i++) {
+            result[i] = getInt(prompt);
+        }
+        return result;
+    }
+
+    static int[][] getIntArraydbl() {
+        int size = IO.getInt("Enter the array size", 0);
+        int[][] arr = new int[size][size];
+
+        for (int row = 0; row< arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                String s = String.format("Enter value for %d, %d", row, col);
+                arr[row][col] = IO.getInt(s);
+            }
+        }
+        return arr;
+    }
+
+    static int getInt(String prompt, int from, int to){
+        int result;
+        do {
+            result = getInt(prompt);
+        }while (result < from || result > to);
+        return result;
+    }
+
+    static int getInt(String prompt, int from){
+        return getInt(prompt, from, Integer.MAX_VALUE);
+    }
+}
---
+++ src/edu/tomer/java/Main.java
@@ -0,0 +1,13 @@
+package edu.tomer.java;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[][] arr = IO.getIntArraydbl();
+        IO.print(arr);
+    }
+
+
+}
